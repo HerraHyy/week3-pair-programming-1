6369363289b21a97bbc1605c8dcbb64cf3dc8135
HerraHyy <tomi.rantakaulio@metropolia.fi>
2024-01-31 18:10:27 +0200

    pagelinks

--- frontend/starter/src/components/Navbar.js
+++ frontend/starter/src/components/Navbar.js
@@ -31,38 +31,7 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
-        <ul className="nav-icons">
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              rel="noreferrer"
-              className="nav-icon"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              rel="noreferrer"
-              className="nav-icon"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              rel="noreferrer"
-              className="nav-icon"
-            >
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li>
-        </ul>
+        <PageLinks parentClass='nav-links' itemClass='nav-link' />
       </div>
     </nav>
   );
